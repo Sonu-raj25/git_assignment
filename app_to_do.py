9a45fcb2027d6cc80b2fb796260cbc51517a9e80
Sonu Raj <sonuraj100101@gmai.com>
2025-07-25 12:05:58 +0000

    Added Item UUID field to To-Do form

--- app_to_do.py
+++ app_to_do.py
@@ -26,14 +26,17 @@ def submit_todo_item():
     item_name = request.form.get('item_name')  # <-- changed here
     item_desc = request.form.get('itemDescription')
     item_id = request.form.get('itemID')
+    item_uuid = request.form.get('itemUUID')
 
     if item_name and item_desc and item_id:
         collection.insert_one({
             "item_name": item_name,
             "item_desc": item_desc,
-            "item_id": item_id
+            "item_id": item_id,
+            "item_uuid": item_uuid
+
         })
-        print(f"Item Name: {item_name}, Item Description: {item_desc}, Item ID: {item_id}")
+        print(f"Item Name: {item_name}, Item Description: {item_desc}, Item ID: {item_id}, Item UUID: {item_uuid}")
         return "Item added successfully!"
     else:
         return "Missing data", 400
